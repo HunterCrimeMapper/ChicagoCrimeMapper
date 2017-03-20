3fadaa58e835e6dd2e6d34e49ab9592d6bd0d4d1
Galil Gertner <galilg@gmail.com>
2017-03-20 18:15:52 -0400

    Zone score 2 now has all zones.

--- CrimeMap.py
+++ CrimeMap.py
@@ -28,7 +28,7 @@ crime_map = folium.Map(location=[41.889179863, -87.633110109],width= '100%',
 
 crime_map.choropleth(geo_path=geo_path, data = zone_data, columns=['ID', 'value'],
 	threshold_scale=[1, 2, 3, 4, 5, 6], key_on ='feature.id',
-	fill_color= 'YlOrRd', fill_opacity=0.7, line_opacity=0.2, legend_name='Zone Scores')
+	fill_color= 'YlOrRd', fill_opacity=0.7, line_opacity=0.2, legend_name='CRIME IS BAD BABY')
 
 '''
 crime_map.choropleth(geo_path=geo_path)
--- scratch.py
+++ scratch.py
@@ -4,15 +4,15 @@ with open("zone_score2.csv", 'w') as outfile:
     outfile.write("ID,value\n")
     for num in range(1, 450):
         outfile.write(str(num)+",0.0\n")
-    for num in range(451, 900):
+    for num in range(450, 900):
         outfile.write(str(num)+",1.0\n")
-    for num in range(901, 1351):
+    for num in range(900, 1351):
         outfile.write(str(num)+",2.0\n")
-    for num in range(1352, 1802):
+    for num in range(1351, 1802):
         outfile.write(str(num)+",3.0\n")
-    for num in range(1803, 2252):
+    for num in range(1802, 2252):
         outfile.write(str(num)+",4.0\n")
 
-    for num in range(2253, 2706):
+    for num in range(2252, 2706):
         outfile.write(str(num)+",5.0\n")
 

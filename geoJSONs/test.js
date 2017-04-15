6d87f04431fe89a6b4d0b0bfefb83ee53347e82c
chen7792 <chen7792@gmail.com>
2017-04-14 20:41:30 -0400

    Add files via upload

---
+++ geoJSONs/test.js
@@ -0,0 +1,5 @@
+ {
+ 	"type": "FeatureCollection",
+ 	"features": [
+ 	{"type": "Feature", "properties": {"id": 1, }, "geometry": {"type": "Polygon", "coordinates": [[[-87.67218800000001, 42.022983], [-87.66718800000001, 42.022983], [-87.66718800000001, 42.017983], [-87.67218800000001, 42.017983]]]
+}}]}

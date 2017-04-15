edaff518e08f8730e797cb123786244281b288a9
chen7792 <chen7792@gmail.com>
2017-04-14 20:32:51 -0400

    Update test.js

--- geoJSONs/test.js
+++ geoJSONs/test.js
@@ -1,5 +1,4 @@
 eqfeed_callback( 
 	{"type": "FeatureCollection", "features": 
-	[{"id": 1, "type": "Feature", "properties": {}, 
-	"geometry": {"type": "Polygon", "coordinates": [[[-87.677188, 42.022983], [-87.672188, 42.022983], [-87.672188, 42.017983], [-87.677188, 42.017983]]]
+	{"id": 2, "type": "Feature", "properties": {}, "geometry": {"type": "Polygon", "coordinates": [[[-87.672188, 42.022983], [-87.66718800000001, 42.022983], [-87.66718800000001, 42.017983], [-87.672188, 42.017983]]]
 }}]});

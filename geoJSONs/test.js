dc79e5699add5aab7d33c8788964e8c6a9fff7b2
chen7792 <chen7792@gmail.com>
2017-04-14 20:34:29 -0400

    Delete test.js

--- geoJSONs/test.js
+++
@@ -1,4 +0,0 @@
-eqfeed_callback( 
-	{"type": "FeatureCollection", "features": 
-	{"id": 2, "type": "Feature", "properties": {}, "geometry": {"type": "Polygon", "coordinates": [[[-87.672188, 42.022983], [-87.66718800000001, 42.022983], [-87.66718800000001, 42.017983], [-87.672188, 42.017983]]]
-}}]});

3010f79fa85c9593b72cb5bc98f7cab1b7bd579a
chen7792 <chen7792@gmail.com>
2017-04-14 20:30:39 -0400

    Add files via upload
    test zones for google map

---
+++ geoJSONs/test.js
@@ -0,0 +1,5 @@
+eqfeed_callback( 
+	{"type": "FeatureCollection", "features": 
+	[{"id": 1, "type": "Feature", "properties": {}, 
+	"geometry": {"type": "Polygon", "coordinates": [[[-87.677188, 42.022983], [-87.672188, 42.022983], [-87.672188, 42.017983], [-87.677188, 42.017983]]]
+}}]});

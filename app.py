b50614c8cd8c148113cb6a71b35dc29f844de2b2
Galil Gertner <galilg@gmail.com>
2017-04-20 11:09:18 -0400

    Cleanup.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json, jsonify
 from flask_googlemaps import GoogleMaps, Map
 from lib.api.zoning import Q2J
 
-import json
+#import json
 
 app = Flask(__name__, template_folder="./templates")
 
@@ -20,19 +20,26 @@ def get_map():
 
     return render_template('googlemap.html')
 
-@app.route('/crime_data', methods=['GET'])
+@app.route('/crime_data', methods= ['GET', 'POST'])
 def get_crime_data():
     #import pdb; pdb.set_trace()
     print("HELLLOOOO!!!")
-    query = Q2J.QueryToJSON(2704)
-    query.load_data_frame('/Users/galil/src/crime_mapper/lib/api/zoning/2017assault.csv')
-    data = query.make_percentile_map()
-    response = app.response_class(
-        response=json.dumps(data),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
+    info = None
+    if request.method == 'POST':
+        print("Made it into POST")
+        info = request.json
+        print(info)
+        return jsonify(info)
+        #response = app.response_class(
+        #    response=json.dumps(info),
+        #    status=200,
+        #    mimetype='application/json')
+    else:
+        print("Not ran not in post")
+        return ("Hi there")
+    #query = Q2J.QueryToJSON(2704)
+    #query.load_data_frame('/Users/galil/src/crime_mapper/lib/api/zoning/2017assault.csv')
+    #data = query.make_percentile_map()
 
 
 if __name__ == "__main__":
